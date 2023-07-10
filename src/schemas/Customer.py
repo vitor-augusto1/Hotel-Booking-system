347ba69f0fd4abcf1e535f1e9928592d026674a2
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-10 17:17:51 -0300

    feat: implement `Customer` schema

---
+++ src/schemas/Customer.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from pydantic import BaseModel
+from uuid import UUID, uuid4
+
+
+class Customer(BaseModel):
+    id: UUID = uuid4()
+    first_name: str
+    middle_name: Optional[str]
+    last_name: str
+    password: str
+    booking_id: UUID
