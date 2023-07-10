4689c61c8c863f684bd5f98db9596aada6cdffb4
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-10 17:17:42 -0300

    feat: Implement `Booking` schema

---
+++ src/schemas/Booking.py
@@ -0,0 +1,11 @@
+from datetime import date
+from typing import List
+from pydantic import BaseModel
+from uuid import UUID, uuid4
+
+
+class Booking(BaseModel):
+    id: UUID = uuid4()
+    rooms: List[UUID]
+    start_date: date
+    end_date: date
