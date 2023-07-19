7cab7e68a8f6f0ccb8ccacafa564cb9a357ef4cd
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-19 12:57:14 -0300

    feat: Create new route to return a room by it's ID

--- src/routes/rooms_router.py
+++ src/routes/rooms_router.py
@@ -8,6 +8,10 @@ from flask import Blueprint, request, escape
 room_endpoint = Blueprint('rooms', __name__)
 
 
+@room_endpoint.route('/room/<uuid:id>/', methods=['GET'])
+def show_room_by_id(id: UUID):
+    room: Room = db.return_room_by_id(id)
+    return {"room": room}, 200
 
 
 @room_endpoint.route('/room/', methods=['GET'])
