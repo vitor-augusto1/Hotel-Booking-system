fd25d1bc2082ca00ea2495cedba2b1740627a4a8
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-19 12:58:27 -0300

    feat: Add the rooms endpoints

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.user_auth_router import user_auth_endpoint
+from routes.rooms_router import room_endpoint
 
 app: Flask = Flask(__name__)
 app.register_blueprint(user_auth_endpoint)
+app.register_blueprint(room_endpoint)
 
 
 @app.route("/")
