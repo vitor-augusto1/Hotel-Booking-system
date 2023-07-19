13361c51c6225a4746e07ef80edcbcf7506037fb
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-19 12:23:36 -0300

    feat: Create new method to create the room's table

--- src/database/connection.py
+++ src/database/connection.py
@@ -9,6 +9,14 @@ class Database:
         self.db_instance = sqlite3.connect("hotel_system.db", check_same_thread=False)
         self.cursor = self.db_instance.cursor()
 
+    def create_rooms_table(self) -> None:
+        query = f"""
+        CREATE TABLE rooms (id text, room_status integer, room_type text, price real)
+        """
+        self.cursor.execute(query)
+        self.db_instance.commit()
+        print("Table successfully created")
+
 db: Database = Database()
 
 
