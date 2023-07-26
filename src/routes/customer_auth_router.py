aa15a322fd333db52dfa43cc3ce26c7e71ecdcf2
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-26 13:53:54 -0300

    feat: Create new route to register the customers

---
+++ src/routes/customer_auth_router.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+from controller.customer_auth_controller import create_new_customer
+
+
+customer_auth_endpoint = Blueprint('auth', __name__)
+
+customer_auth_endpoint.route('/register/', methods=['POST'])(create_new_customer)
