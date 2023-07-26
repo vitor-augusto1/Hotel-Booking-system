462cf148e6c73130f14ebb58b2b295110b936ff0
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-26 13:55:26 -0300

    feat: Create new util function to check the customer password

--- src/utils/encrypt.py
+++ src/utils/encrypt.py
@@ -9,3 +9,7 @@ def encrypt_password(clean_password: str) -> bytes:
     return hash
 
 
+def check_password(provided_password: str, stored_password_hash: bytes) -> bool:
+    provided_password_bytes: bytes = provided_password.encode('utf-8')
+    result: bool = bcrypt.checkpw(provided_password_bytes, stored_password_hash)
+    return result
