65fbfe9ed3abb647cf08e68b8f916f373f36f5c9
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-07-26 13:55:47 -0300

    feat: Add a new customer auth endpoint

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from routes.user_auth_router import user_auth_endpoint
 from routes.rooms_router import room_endpoint
+from routes.customer_auth_router import customer_auth_endpoint
 
 app: Flask = Flask(__name__)
-app.register_blueprint(user_auth_endpoint)
 app.register_blueprint(room_endpoint)
+app.register_blueprint(customer_auth_endpoint)
 
 
 @app.route("/")
