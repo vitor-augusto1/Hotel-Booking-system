9aa5dc4674361e17501bbc165a400547931d72ce
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-03 15:00:46 -0300

    feat: Create new controller to authenticate the customer

--- src/controller/customer_auth_controller.py
+++ src/controller/customer_auth_controller.py
@@ -1,7 +1,14 @@
+from typing import Optional
+from uuid import UUID
 from flask import request
+import jwt
 from schemas.Customer import Customer
-from utils.encrypt import encrypt_password
+from schemas.Token import TokenPayload
+from utils.encrypt import encrypt_password, check_password
 from database.connection import db
+
+
+jwt_secret: str = "my_secret_here"
 
 
 def create_new_customer():
@@ -29,3 +36,23 @@ def create_new_customer():
     return {
             "success": "Customer created successfully",
            }, 200
+
+
+def authenticate_user():
+    args = request.form
+    email = args.get('email')
+    password = args.get('password')
+    customer: Optional[Customer] = db.find_customer_by_email(email)
+    if customer is None:
+        return {"user": "Not found"}, 404
+    customer_stored_password: str = customer[0][5]
+    user_provided_a_valid_password: bool = check_password(
+            password, customer_stored_password.encode('utf8')
+    )
+    if (not user_provided_a_valid_password):
+        return {"error": "Invalid Credentials"}, 401
+    customer_id: UUID = customer[0][0]
+    jwt_payload: TokenPayload = TokenPayload(customer_id=customer_id)
+    token = jwt.encode(jwt_payload.dict(), jwt_secret, algorithm="HS256")
+    print(f"Generated token: {token}")
+    return {"success": token}, 200
