2905bb002ea801e54bd83bec53ceeecd24088500
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-03 15:02:42 -0300

    feat: Add the new 'authenticate_user' controller to the '/login' endpoint

--- src/routes/customer_auth_router.py
+++ src/routes/customer_auth_router.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from controller.customer_auth_controller import create_new_customer
+from controller.customer_auth_controller import create_new_customer, authenticate_user
 
 
 customer_auth_endpoint = Blueprint('auth', __name__)
 
 customer_auth_endpoint.route('/register/', methods=['POST'])(create_new_customer)
+customer_auth_endpoint.route('/login/', methods=['POST'])(authenticate_user)
