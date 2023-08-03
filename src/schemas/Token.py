bf91ee7f2ba31668ace1eb3b2801d208922b8130
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-03 15:03:21 -0300

    feat: Add new 'Token' pydantic schema

---
+++ src/schemas/Token.py
@@ -0,0 +1,9 @@
+import datetime
+import uuid
+from pydantic import BaseModel
+from uuid import UUID
+
+
+class TokenPayload(BaseModel):
+    customer_id: str
+    exp: datetime.datetime = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
