98972635cde3c8b50c0987da3eaa0a51d5aaa9e8
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-13 19:07:44 -0300

    feat: Add the admin's endpoints to the app

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from flask import Flask
 from routes.rooms_router import room_endpoint
 from routes.customer_auth_router import customer_auth_endpoint
 from routes.book_router import book_endpoint
+from routes.admin_router import admin_endpoints
 
 app: Flask = Flask(__name__)
 app.register_blueprint(room_endpoint)
 app.register_blueprint(customer_auth_endpoint)
 app.register_blueprint(book_endpoint)
+app.register_blueprint(admin_endpoints)
 
 
 @app.route("/")
