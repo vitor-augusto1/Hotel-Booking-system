3aeec5421c39176ef8e87924e1e871d685b2a8a7
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-12 22:01:56 -0300

    feat: Add new book endpoint

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from routes.rooms_router import room_endpoint
 from routes.customer_auth_router import customer_auth_endpoint
+from routes.book_router import book_endpoint
 
 app: Flask = Flask(__name__)
 app.register_blueprint(room_endpoint)
 app.register_blueprint(customer_auth_endpoint)
+app.register_blueprint(book_endpoint)
 
 
 @app.route("/")
