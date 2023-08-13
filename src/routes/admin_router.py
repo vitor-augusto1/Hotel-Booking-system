7c9214d710747ab0a09a187f76f452361dfc847c
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-13 19:05:02 -0300

    feat: Create new admin route to view all customers and bookings

---
+++ src/routes/admin_router.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+from controller.admin_controller import return_all_customers, return_all_bookings
+
+admin_endpoints = Blueprint('admin', __name__)
+
+
+admin_endpoints.route('/customers/', methods=['GET'])(return_all_customers)
+admin_endpoints.route('/bookings/', methods=['GET'])(return_all_bookings)
