ede3873219b7d03c8ad4f5472d21abb2707fbe64
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-14 15:45:44 -0300

    feat: Add new 'admin_only' decorator for user authorization

--- src/decorators/auth_decorator.py
+++ src/decorators/auth_decorator.py
@@ -26,3 +26,24 @@ def login_required(func):
     return decorated
 
 
+def admin_only(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = None
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split(" ")[1]
+        if not token:
+            return {"error": "Unauthorized"}, 401
+        try:
+            data: TokenPayload = jwt.decode(token, "my_secret_here", algorithms=["HS256"])
+            customer: Customer = db.find_customer_by_id(data['customer_id'])
+            print(f"Customer: {customer}")
+            if customer is None:
+                return {"error": "Invalid authentication token"}, 401
+            is_customer_not_admin: bool = not data['is_admin']
+            if is_customer_not_admin:
+                return {"error": "Unauthorized"}, 403
+        except Exception as e:
+            return {"error": f"Something went wrong: {e}"}, 500
+        return func(*args, **kwargs)
+    return decorated
