1242bcb78721cbabf65363ff31def3dab5d4093f
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-14 15:45:12 -0300

    refactor: Remove brasilia timezone

--- src/decorators/auth_decorator.py
+++ src/decorators/auth_decorator.py
@@ -4,8 +4,6 @@ import jwt
 from schemas.Token import TokenPayload
 from schemas.Customer import Customer
 from database.connection import db
-import datetime
-import pytz
 
 
 def login_required(func):
@@ -22,13 +20,9 @@ def login_required(func):
             print(f"Customer: {customer}")
             if customer is None:
                 return {"error": "Invalid authentication token"}, 401
-            utc_now = datetime.datetime.utcnow().replace(tzinfo=None)
-            brasilia_timezone = pytz.timezone('America/Sao_Paulo')
-            utc_now_brasilia_timezone = utc_now.astimezone(brasilia_timezone).replace(tzinfo=None)
-            token_expire_date = data["exp"]
-            if utc_now_brasilia_timezone > datetime.datetime.fromtimestamp(token_expire_date):
-                return {"error": "Expired token"}, 401
         except Exception as e:
             return {"error": f"Something went wrong: {e}"}, 500
         return func(*args, **kwargs)
     return decorated
+
+
